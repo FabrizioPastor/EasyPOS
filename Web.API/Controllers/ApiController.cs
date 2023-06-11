3aeef8cee664d3c729d79c9a5fdd1a31a666a728
fabriziopastor <fabriziopastors@outlook.com>
2023-06-11 15:48:58 -0500

    refactor ApiController

--- Web.API/Controllers/ApiController.cs
+++ Web.API/Controllers/ApiController.cs
@@ -1,17 +1,25 @@
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Web.API.Commons.Http;
 
 namespace Web.API.Controllers;
 
-public class ApiController: ControllerBase
+[ApiController]
+public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
-        if (errors.Count is 0)
+        if(errors.Count is 0)
+        {
             return Problem();
-        if (errors.All(error => error.Type == ErrorType.Validation))
+        }
+
+        if(errors.All(error => error.Type == ErrorType.Validation))
+        {
             return ValidationProblem(errors);
+        }
+
         HttpContext.Items[HttpContextItemKeys.Errors] = errors;
 
         return Problem(errors[0]);
@@ -19,15 +27,26 @@ public class ApiController: ControllerBase
 
     private IActionResult Problem(Error error)
     {
-        var statusCode = error.Type switch
+        var statusCode = error.Type switch 
         {
-            ErrorType.Conflict => StatusCode.Status409Conflict,
-            ErrorType.Validation => StatusCode.Status400BadRequest,
-            ErrorType.NotFound => StatusCode.Status404NotFound,
-            _ => StatusCode.Status500InternalServerError,
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError,
         };
+
         return Problem(statusCode: statusCode, title: error.Description);
     }
-    
-    private IActionResult ValidationProblem()
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(error.Code, error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
+    }
 }
